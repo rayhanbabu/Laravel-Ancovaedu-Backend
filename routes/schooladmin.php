0ac03c98ce261ee2ef74ad9ad5147b6fd4d3785e
rayhanbabu <rayhanbabu458@gmail.com>
2025-04-27 22:09:29 +0600

    fix: correct namespace casing for SchoolAdminController

--- routes/schooladmin.php
+++ routes/schooladmin.php
@@ -1,6 +1,6 @@
 <?php 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\schoolAdmin\SchoolAdminController;
+use App\Http\Controllers\SchoolAdmin\SchoolAdminController;
 
   Route::middleware('auth:sanctum')->group(function () {
       Route::middleware('Supperadmin')->group(function () {
